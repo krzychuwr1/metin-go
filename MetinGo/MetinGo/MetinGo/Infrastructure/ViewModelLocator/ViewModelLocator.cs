ec519b09a547223842edb17e3a0a32b371f7d7bc
Krzysztof Wrona <krzychuwr1@gmail.com>
2018-03-19 20:50:50 +0100

    initial commit

---
+++ MetinGo/MetinGo/MetinGo/Infrastructure/ViewModelLocator/ViewModelLocator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MetinGo.ViewModels;
+
+namespace MetinGo.Infrastructure.ViewModelLocator
+{
+    public class ViewModelLocator
+    {
+		public static ItemsViewModel ItemsViewModel => new ItemsViewModel();
+	}
+}
