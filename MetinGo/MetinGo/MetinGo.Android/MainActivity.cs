a55a516ef8375d9a7d3b4c4332140571b7b7cb2d
Krzysztof Wrona <krzychuwr1@gmail.com>
2018-05-26 16:46:05 +0200

    new app icon

--- MetinGo/MetinGo/MetinGo.Android/MainActivity.cs
+++ MetinGo/MetinGo/MetinGo.Android/MainActivity.cs
@@ -13,7 +13,7 @@ using Xamarin.Forms;
 
 namespace MetinGo.Droid
 {
-    [Activity(Label = "Metin Go", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Metin Go", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
