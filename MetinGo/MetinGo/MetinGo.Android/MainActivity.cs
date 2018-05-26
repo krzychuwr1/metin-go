09207cec32d9c5b48db403b9be77e8b4c8e30482
Krzysztof Wrona <krzychuwr1@gmail.com>
2018-05-26 17:24:51 +0200

    Add splash screen

--- MetinGo/MetinGo/MetinGo.Android/MainActivity.cs
+++ MetinGo/MetinGo/MetinGo.Android/MainActivity.cs
@@ -13,7 +13,7 @@ using Xamarin.Forms;
 
 namespace MetinGo.Droid
 {
-    [Activity(Label = "Metin Go", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
---
+++ MetinGo/MetinGo/MetinGo.Android/SplashActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+
+namespace MetinGo.Droid
+{
+    [Activity(Theme = "@style/MainTheme.Splash", MainLauncher = true, NoHistory = true, Label = "Metin Go", Icon = "@mipmap/icon", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
